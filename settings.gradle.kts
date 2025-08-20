179e72e332472d6463e456a3522f19a500a8034c
Bogdan Patseev <patseev1987@gmail.com>
2025-08-20 22:40:18 +0300

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Department observer"
+include(":app")
+include(":modules")
+include(":modules:features")
+include(":modules:core")
+include(":modules:base")
+include(":modules:ui")
+
